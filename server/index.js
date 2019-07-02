ab5c4858beb06dc225e527317595664ed986dc8b
John Park <jhnprk12@gmail.com>
2019-07-02 09:40:59 -0700

    circle ci

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const server = require('./app');
 require('dotenv').config();
+const PORT = process.env.PORT;
 
-server.listen(8080, function() {
-  console.log('Server is listening on http://localhost:8080');
+server.listen(PORT, function() {
+  console.log('Server is listening on http://localhost:${PORT}');
 });
