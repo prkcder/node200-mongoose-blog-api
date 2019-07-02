42ff6bd97addbe006bcd2addaebd81d4be28a933
John Park <jhnprk12@gmail.com>
2019-07-02 09:44:26 -0700

    change circle ci

--- server/app.js
+++ server/app.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const morgan = require('morgan');
+// const morgan = require('morgan');
 require('dotenv').config();
 // const dbpassword = process.env.dbpassword;
 
 mongoose.connect('mongodb://test:test12@ds245927.mlab.com:45927/heroku_kbkk9tb3', { useNewUrlParser: true });
 mongoose.Promise = Promise;
 
-app.use(morgan('dev'));
 const app = express();
 
 
+// app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use('/api/users', require('./routes/users'));
 app.use('/api/blogs', require('./routes/blogs'));
